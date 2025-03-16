247983519db9f6dbdc459591ad6fdfa6a39344c9
kaveeshabasnayaka <139634637+kaveeshabasnayaka@users.noreply.github.com>
2025-03-16 16:01:40 +0000

    create codes in Three

--- Three.java
+++ Three.java
@@ -1,1 +1,16 @@
 //3)print the numbers in reverse
+
+public class Three{
+    public static void main(String args[]){
+        int num = 12345;
+        int reverse = 0;
+        int temp;
+
+        while (num>0){
+            temp = num % 10;
+            reverse = reverse * 10 + temp;
+            num = num / 10;
+        }
+        System.out.println("revrese number : "+ reverse);
+    }
+}
