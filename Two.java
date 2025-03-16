d8e8f10bc348560d88744ad85971fa4a34694e46
kaveeshabasnayaka <139634637+kaveeshabasnayaka@users.noreply.github.com>
2025-03-16 15:29:57 +0000

    create codes in Two.js

--- Two.java
+++ Two.java
@@ -1,1 +1,10 @@
 //2)print numbers from 1 to 10
+
+public class Two{
+    public static void main(String args[]){
+
+        for (int i=1;i<=10;i++){
+            System.out.println(i);
+        }
+    }
+}
