8346475c726eef2ed6fbf337f5c5c16481718957
kaveeshabasnayaka <139634637+kaveeshabasnayaka@users.noreply.github.com>
2025-03-16 16:17:55 +0000

    create codes in Four

--- Four.java
+++ Four.java
@@ -1,1 +1,11 @@
 //4)print the odd numbers between 1 to 20
+
+public class Four{
+    public static void main(String args[]){
+        for (int i=1;i<=20;i++){
+            if(i % 2 == 1){
+                System.out.println(i);
+            }
+        }
+    }
+}
