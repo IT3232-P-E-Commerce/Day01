b8207385ac43d13036336aca8c16a629173da7b3
kaveeshabasnayaka <139634637+kaveeshabasnayaka@users.noreply.github.com>
2025-03-16 17:04:54 +0000

    create codes in Five

--- Five.java
+++ Five.java
@@ -5,3 +5,31 @@
 
 //input 13
 //output 6 5 4 3 2 1 7 13 12 11 10 9 8
+
+import java.util.Scanner;
+
+public class Five{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number :");
+        int n = sc.nextInt();
+
+        int mid = n/2+1;
+
+        for(int i=1;i<=n;i++){
+            if(i < mid){
+                System.out.print(mid-i);
+            }
+            else if(i == mid){
+                System.out.print(i);
+            }
+            else{
+                System.out.print(n-i+mid+1);
+            }
+
+        }
+
+
+
+    }
+}
